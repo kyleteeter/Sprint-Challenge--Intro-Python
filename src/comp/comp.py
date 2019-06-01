e5768b6d07ac8a92f739ef097f9d14aebc4b33e7
unknown <kyleteeter12@gmail.com>
2019-06-01 12:31:21 -0500

    Starts with D is working

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -1,5 +1,7 @@
 # The following list comprehension exercises will make use of the 
 # defined Human class. 
+import math
+import re
 class Human:
     def __init__(self, name, age):
         self.name = name
@@ -24,9 +26,9 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [x.name for x in humans if x.name.startswith('D')]
 print(a)
-
+ 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
